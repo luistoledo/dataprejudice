4f26e4b567f6f0b545f346b5696fc896a1b3fbaf
Luis <luis@Patolli.local>
2016-12-22 21:55:09 +0100

    Add prototype

---
+++ prototype_files/resources/story.js
@@ -0,0 +1,107 @@
+var story = {
+ "pages": [
+  {
+   "image": "splash.png",
+   "width": 4267,
+   "links": [{
+    "rect": [
+     1711,
+     1465,
+     2631,
+     1687
+    ],
+    "page": 1
+   }],
+   "title": "splash",
+   "height": 2042
+  },
+  {
+   "image": "new.png",
+   "width": 4267,
+   "links": [
+    {
+     "rect": [
+      2874,
+      965,
+      2986,
+      1123
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      3006,
+      734,
+      3126,
+      832
+     ],
+     "page": 3
+    }
+   ],
+   "title": "new",
+   "height": 2042
+  },
+  {
+   "image": "selection_right.png",
+   "width": 4267,
+   "links": [{
+    "rect": [
+     2360,
+     1864,
+     3144,
+     1992
+    ],
+    "page": 4
+   }],
+   "title": "selection_right",
+   "height": 2042
+  },
+  {
+   "image": "selection_wrong.png",
+   "width": 4267,
+   "links": [{
+    "rect": [
+     2360,
+     1864,
+     3144,
+     1992
+    ],
+    "page": 5
+   }],
+   "title": "selection_wrong",
+   "height": 2042
+  },
+  {
+   "image": "score_right.png",
+   "width": 4267,
+   "links": [{
+    "rect": [
+     2041,
+     1712,
+     2945,
+     1896
+    ],
+    "page": 1
+   }],
+   "title": "score_right",
+   "height": 2042
+  },
+  {
+   "image": "score_wrong.png",
+   "width": 4267,
+   "links": [{
+    "rect": [
+     2041,
+     1712,
+     2945,
+     1896
+    ],
+    "page": 1
+   }],
+   "title": "score_wrong",
+   "height": 2042
+  }
+ ],
+ "title": "prototype",
+ "highlightLinks": false
+}
